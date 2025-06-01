f62690dbc47a56be4bd78a6c27268cde2a96ff6b
Felipe Baltor <fbaltor@gmail.com>
2025-06-01 20:28:13 -0300

    feat: authenticating and getting basic info

---
+++ main.js
@@ -0,0 +1,11 @@
+import { initStarkbank } from "./utils.js";
+
+const starkbank = await initStarkbank();
+
+(async () => {
+  const workspaces = await starkbank.workspace.query();
+
+  for await (const workspace of workspaces) {
+    console.log(workspace);
+  }
+})();
